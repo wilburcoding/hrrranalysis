f2f0150db317cec51ffafaf8638b6457740adcdc
Wilbur W <wilburwei.2008@gmail.com>
2025-07-22 18:44:27 -0400

    Serve generated images in flask application

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, send_file, request
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return "Hello World!"
+
+
+@app.route("/data/<run>/<hour>.png")
+def serve_data(run, hour):
+  return send_file("./data/" + run + "/" + hour + ".png", mimetype='image/png')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0')
